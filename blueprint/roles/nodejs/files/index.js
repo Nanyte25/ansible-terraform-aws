5cab1bab1a7f345e7b15ba7ae0088510438b9bdc
ciaranRoche <ciaranroche@hotmail.co.uk>
2019-04-16 18:24:58 +0100

    remove unused files

--- blueprint/roles/nodejs/files/index.js
+++
@@ -1,8 +0,0 @@
-const http = require("http")
-
-http.createServer(function (request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello World! Cloud Computing!\n');
-}).listen(80);
-
-console.log("Hello World HTTP server ready on port 80.")
